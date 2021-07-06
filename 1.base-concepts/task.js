7c6dbb60a0cd22553b001a84097e3cfbf02cb383
Elena <Fingali_ilio@mail.ru>
2021-07-06 19:58:27 +0500

    первая попытка в решении задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let arr = [];
+  let D = b**2-4*a*c;
+  let x;
+  let x1;
+  let x2;
+  
+  if(D < 0){
+    return arr [0] = 'корней нет';
+  } else if (D===0){
+    return arr [x=-b/(2*a)];
+  } else if (D>0){
+    return arr [(x1 = (-b + Math.sqrt(D))/2*a) && (x2 = (-b - Math.sqrt(D))/(2*a))]; 
+  }
+    return arr; // array
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
