208c0be2ec966220b7226bafc6620c52c7bc5ee0
Elena <Fingali_ilio@mail.ru>
2021-07-08 17:49:48 +0500

    после второй доработки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,17 +5,20 @@ function solveEquation(a, b, c) {
   let x;
   let x1;
   let x2;
-  arr.push(x,x1,x2);
-  if(D<0){
-    arr [0] = 'корней нет';
-  } else if (D===0){
-    arr [0] = -b/(2*a);
-  } else if (D>0) {
-    arr [1] = (-b + Math.sqrt(D))/(2*a);
-    arr [2] = (-b - Math.sqrt(D))/(2*a);
+  arr.push(x1,x2);
+  if (D>0) {
+     arr[0] = (-b + Math.sqrt(D))/(2*a);
+     arr[1] = (-b - Math.sqrt(D))/(2*a);
   };
-    return arr; // array
+  if (D===0){
+    return [x=-b/(2*a)];
+  };
+  if (D<0){
+    return [];
+  }
+  return arr; // array
 };
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
