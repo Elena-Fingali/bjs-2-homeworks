69b345d44af90d71cb79246aa843987ffe22a207
Elena <Fingali_ilio@mail.ru>
2021-07-10 22:41:37 +0500

    решены 2 задачи

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  let compareItem = [];
+  if(arr1.length === arr2.length && Array.isArray(arr1) === true &&Array.isArray(arr2) === true){
+    result = Array.from(arr1).every((item,index)=>{item = item === arr2[index];
+    return item})
+  } else result = false;
   return result; // boolean
 }
 
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = (arr.filter((item)=> item>0).filter((item)=>item % 3 === 0).map((item) => item * 10));
   return resultArr; // array
 }
